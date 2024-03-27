56289870541f84b4769ea738cafc0e52276b97bf
Rushil Shah <rushilrs@usc.edu>
2024-03-26 21:50:39 -0700

    added Postcard Object Model

--- src/main/java/com/postexchange/model/Postcard.java
+++ src/main/java/com/postexchange/model/Postcard.java
@@ -1,15 +1,82 @@
-/*
- * Author: jianqing
- * Date: Mar 26, 2024
- * Description: This document is created for
- */
 package com.postexchange.model;
 
-/**
- *
- * @author jianqing
- */
-public class Postcard
-{
-    
+public class Postcard {
+    private int postcardID;
+    private String timeSent;
+    private String timeReceived;
+    private int userIDSent;
+    private int userIDReceived;
+    private String postcardImage;
+    private String postcardMessage;
+
+    // Constructors
+    public Postcard() {
+    }
+
+    public Postcard(int postcardID, String timeSent, String timeReceived, int userIDSent, int userIDReceived, String postcardImage, String postcardMessage) {
+        this.postcardID = postcardID;
+        this.timeSent = timeSent;
+        this.timeReceived = timeReceived;
+        this.userIDSent = userIDSent;
+        this.userIDReceived = userIDReceived;
+        this.postcardImage = postcardImage;
+        this.postcardMessage = postcardMessage;
+    }
+
+    // Getters and setters
+    public int getPostcardID() {
+        return postcardID;
+    }
+
+    public void setPostcardID(int postcardID) {
+        this.postcardID = postcardID;
+    }
+
+    public String getTimeSent() {
+        return timeSent;
+    }
+
+    public void setTimeSent(String timeSent) {
+        this.timeSent = timeSent;
+    }
+
+    public String getTimeReceived() {
+        return timeReceived;
+    }
+
+    public void setTimeReceived(String timeReceived) {
+        this.timeReceived = timeReceived;
+    }
+
+    public int getUserIDSent() {
+        return userIDSent;
+    }
+
+    public void setUserIDSent(int userIDSent) {
+        this.userIDSent = userIDSent;
+    }
+
+    public int getUserIDReceived() {
+        return userIDReceived;
+    }
+
+    public void setUserIDReceived(int userIDReceived) {
+        this.userIDReceived = userIDReceived;
+    }
+
+    public String getPostcardImage() {
+        return postcardImage;
+    }
+
+    public void setPostcardImage(String postcardImage) {
+        this.postcardImage = postcardImage;
+    }
+
+    public String getPostcardMessage() {
+        return postcardMessage;
+    }
+
+    public void setPostcardMessage(String postcardMessage) {
+        this.postcardMessage = postcardMessage;
+    }
 }
