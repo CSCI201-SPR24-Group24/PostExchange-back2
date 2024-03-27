b3bf85442ca8accfd3c6963ad49af27a2acb963a
Jianqing Gao <gaojohns@usc.edu>
2024-03-26 20:58:56 -0700

    Initial Commit

---
+++ src/main/java/com/postexchange/model/Postcard.java
@@ -0,0 +1,15 @@
+/*
+ * Author: jianqing
+ * Date: Mar 26, 2024
+ * Description: This document is created for
+ */
+package com.postexchange.model;
+
+/**
+ *
+ * @author jianqing
+ */
+public class Postcard
+{
+    
+}
---
+++ src/main/java/com/postexchange/network/SQLAccessor.java
@@ -0,0 +1,15 @@
+/*
+ * Author: jianqing
+ * Date: Mar 26, 2024
+ * Description: This document is created for
+ */
+package com.postexchange.network;
+
+/**
+ *
+ * @author jianqing
+ */
+public class SQLAccessor
+{
+    
+}
