d4ee7f5c22f587d0f580498c3786aca41d08cb67
Johnson Gao <gaojohns@usc.edu>
2024-04-06 06:37:49 -0700

    Started servlet, provides helpers, example and test codes.

--- src/main/java/com/postexchange/model/User.java
+++ src/main/java/com/postexchange/model/User.java
@@ -13,106 +13,143 @@ public class User
 {
 
     private String userId;
-    private String emailId;
-    private String password;
+    private String email;
+    private transient String password;
     private String firstName;
     private String lastName;
     private String lastLoginTime;
     private int numberSent;
-    private int numberReceived; 
+    private int numberReceived;
     private String userCountry;
     private String userBio;
     private String profilePicture;
+    private String lastDonated;
 
-    public String getUserId() {
+    public String getLastDonated()
+    {
+        return lastDonated;
+    }
+
+    public void setLastDonated(String lastDonated)
+    {
+        this.lastDonated = lastDonated;
+    }
+
+    public String getUserId()
+    {
         return userId;
     }
 
-    public String getEmailId() {
-        return emailId;
+    public String getEmail()
+    {
+        return email;
     }
 
-    public String getPassword() {
+    public String getPassword()
+    {
         return password;
     }
 
-    public String getFirstName() {
+    public String getFirstName()
+    {
         return firstName;
     }
 
-    public String getLastName() {
+    public String getLastName()
+    {
         return lastName;
     }
 
-    public String getLastLoginTime() {
+    public String getLastLoginTime()
+    {
         return lastLoginTime;
     }
 
-    public int getNumberSent() {
+    public int getNumberSent()
+    {
         return numberSent;
     }
 
-    public int getNumberReceived() {
+    public int getNumberReceived()
+    {
         return numberReceived;
     }
 
-    public String getUserCountry() {
+    public String getUserCountry()
+    {
         return userCountry;
     }
 
-    public String getUserBio() {
+    public String getUserBio()
+    {
         return userBio;
     }
 
-    public String getProfilePicture() {
+    public String getProfilePicture()
+    {
         return profilePicture;
     }
 
     // Setters
-    public void setUserId(String userId) {
+    public void setUserId(String userId)
+    {
         this.userId = userId;
     }
 
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
+    public void setEmail(String email)
+    {
+        this.email = email;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password)
+    {
         this.password = password;
     }
 
-    public void setFirstName(String firstName) {
+    public void setFirstName(String firstName)
+    {
         this.firstName = firstName;
     }
 
-    public void setLastName(String lastName) {
+    public void setLastName(String lastName)
+    {
         this.lastName = lastName;
     }
 
-    public void setLastLoginTime(String lastLoginTime) {
+    public void setLastLoginTime(String lastLoginTime)
+    {
         this.lastLoginTime = lastLoginTime;
     }
 
-    public void setNumberSent(int numberSent) {
+    public void setNumberSent(int numberSent)
+    {
         this.numberSent = numberSent;
     }
 
-    public void setNumberReceived(int numberReceived) {
+    public void setNumberReceived(int numberReceived)
+    {
         this.numberReceived = numberReceived;
     }
 
-    public void setUserCountry(String userCountry) {
+    public void setUserCountry(String userCountry)
+    {
         this.userCountry = userCountry;
     }
 
-    public void setUserBio(String userBio) {
+    public void setUserBio(String userBio)
+    {
         this.userBio = userBio;
     }
 
-    public void setProfilePicture(String profilePicture) {
+    public void setProfilePicture(String profilePicture)
+    {
         this.profilePicture = profilePicture;
     }
 
+    @Override
+    public String toString()
+    {
+        return "User{" + "userId=" + userId + ", emailId=" + email + ", password=" + password + ", firstName=" + firstName + ", lastName=" + lastName + ", lastLoginTime=" + lastLoginTime + ", numberSent=" + numberSent + ", numberReceived=" + numberReceived + ", userCountry=" + userCountry + ", userBio=" + userBio + ", profilePicture=" + profilePicture + '}';
+    }
 
-    
 }
